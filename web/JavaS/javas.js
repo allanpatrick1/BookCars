77258ec3ba160f2daf311cde7bdd270f5cb177a9
ALLAN <ALLAN@DESKTOP-NF0AFGD>
2018-03-03 02:25:49 -0300

    Adicionando projeto ao repositorio.

---
+++ web/JavaS/javas.js
@@ -0,0 +1,38 @@
+/* 
+    Created on : 03/03/2018, 01:05:06
+    Author     : https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
+    Edited by  : ALLAN
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
